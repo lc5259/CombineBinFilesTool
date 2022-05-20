05fb6da38a3ff8a82357c7530a4bc6f4ac1df2f8
lc5259 <1433420541@qq.com>
2022-05-20 11:14:04 +0800

    提交v1.0版本

--- CombineBinFilesTool/CombineBinFilesTool/Form1.Designer.cs
+++ CombineBinFilesTool/CombineBinFilesTool/Form1.Designer.cs
@@ -28,13 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BootFileSize = new System.Windows.Forms.TextBox();
+            this.AppFileSize = new System.Windows.Forms.TextBox();
+            this.CRCSize = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBBootFolder = new System.Windows.Forms.TextBox();
+            this.btBoot = new System.Windows.Forms.Button();
+            this.btApp = new System.Windows.Forms.Button();
+            this.tBAppFolder = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btCombineFiles = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label9 = new System.Windows.Forms.Label();
+            this.CRC200 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CRC512 = new System.Windows.Forms.RadioButton();
+            this.labelhide = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " boot loader文件夹下bin文件大小";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CRC段配置空间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "app文件夹下bin文件大小";
+            // 
+            // BootFileSize
+            // 
+            this.BootFileSize.Location = new System.Drawing.Point(258, 39);
+            this.BootFileSize.Name = "BootFileSize";
+            this.BootFileSize.Size = new System.Drawing.Size(100, 21);
+            this.BootFileSize.TabIndex = 5;
+            this.BootFileSize.Enter += new System.EventHandler(this.BootFileSize_Enter);
+            this.BootFileSize.Leave += new System.EventHandler(this.BootFileSize_Leave);
+            // 
+            // AppFileSize
+            // 
+            this.AppFileSize.Location = new System.Drawing.Point(258, 72);
+            this.AppFileSize.Name = "AppFileSize";
+            this.AppFileSize.Size = new System.Drawing.Size(100, 21);
+            this.AppFileSize.TabIndex = 6;
+            this.AppFileSize.Enter += new System.EventHandler(this.AppFileSize_Enter);
+            this.AppFileSize.Leave += new System.EventHandler(this.AppFileSize_Leave);
+            // 
+            // CRCSize
+            // 
+            this.CRCSize.Location = new System.Drawing.Point(258, 138);
+            this.CRCSize.Name = "CRCSize";
+            this.CRCSize.Size = new System.Drawing.Size(100, 21);
+            this.CRCSize.TabIndex = 7;
+            this.CRCSize.Enter += new System.EventHandler(this.CRCSize_Enter);
+            this.CRCSize.Leave += new System.EventHandler(this.CRCSize_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = " boot loader文件夹下bin文件：";
+            // 
+            // tBBootFolder
+            // 
+            this.tBBootFolder.Location = new System.Drawing.Point(182, 192);
+            this.tBBootFolder.Name = "tBBootFolder";
+            this.tBBootFolder.Size = new System.Drawing.Size(302, 21);
+            this.tBBootFolder.TabIndex = 9;
+            // 
+            // btBoot
+            // 
+            this.btBoot.Location = new System.Drawing.Point(490, 192);
+            this.btBoot.Name = "btBoot";
+            this.btBoot.Size = new System.Drawing.Size(60, 23);
+            this.btBoot.TabIndex = 10;
+            this.btBoot.Text = "选择";
+            this.btBoot.UseVisualStyleBackColor = true;
+            this.btBoot.Click += new System.EventHandler(this.btBoot_Click);
+            // 
+            // btApp
+            // 
+            this.btApp.Location = new System.Drawing.Point(491, 233);
+            this.btApp.Name = "btApp";
+            this.btApp.Size = new System.Drawing.Size(60, 23);
+            this.btApp.TabIndex = 13;
+            this.btApp.Text = "选择";
+            this.btApp.UseVisualStyleBackColor = true;
+            this.btApp.Click += new System.EventHandler(this.btApp_Click);
+            // 
+            // tBAppFolder
+            // 
+            this.tBAppFolder.Location = new System.Drawing.Point(182, 233);
+            this.tBAppFolder.Name = "tBAppFolder";
+            this.tBAppFolder.Size = new System.Drawing.Size(302, 21);
+            this.tBAppFolder.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "app文件夹下bin文件：";
+            // 
+            // btCombineFiles
+            // 
+            this.btCombineFiles.Location = new System.Drawing.Point(267, 271);
+            this.btCombineFiles.Name = "btCombineFiles";
+            this.btCombineFiles.Size = new System.Drawing.Size(84, 32);
+            this.btCombineFiles.TabIndex = 14;
+            this.btCombineFiles.Text = "文件合并";
+            this.btCombineFiles.UseVisualStyleBackColor = true;
+            this.btCombineFiles.Click += new System.EventHandler(this.btCombineFiles_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(360, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "kb";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(360, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "kb";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(359, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "kb";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(157, 108);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 12);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "CRC校验字段大小";
+            // 
+            // CRC200
+            // 
+            this.CRC200.AutoSize = true;
+            this.CRC200.Checked = true;
+            this.CRC200.Location = new System.Drawing.Point(6, 9);
+            this.CRC200.Name = "CRC200";
+            this.CRC200.Size = new System.Drawing.Size(47, 16);
+            this.CRC200.TabIndex = 20;
+            this.CRC200.TabStop = true;
+            this.CRC200.Text = "200k";
+            this.CRC200.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CRC512);
+            this.groupBox1.Controls.Add(this.CRC200);
+            this.groupBox1.Location = new System.Drawing.Point(258, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(118, 31);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            // 
+            // CRC512
+            // 
+            this.CRC512.AutoSize = true;
+            this.CRC512.Location = new System.Drawing.Point(63, 9);
+            this.CRC512.Name = "CRC512";
+            this.CRC512.Size = new System.Drawing.Size(47, 16);
+            this.CRC512.TabIndex = 21;
+            this.CRC512.Text = "512k";
+            this.CRC512.UseVisualStyleBackColor = true;
+            // 
+            // labelhide
+            // 
+            this.labelhide.AutoSize = true;
+            this.labelhide.Location = new System.Drawing.Point(448, 75);
+            this.labelhide.Name = "labelhide";
+            this.labelhide.Size = new System.Drawing.Size(101, 12);
+            this.labelhide.TabIndex = 22;
+            this.labelhide.Text = "用于失去焦点功能";
+            this.labelhide.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(571, 354);
+            this.Controls.Add(this.labelhide);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btCombineFiles);
+            this.Controls.Add(this.btApp);
+            this.Controls.Add(this.tBAppFolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btBoot);
+            this.Controls.Add(this.tBBootFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CRCSize);
+            this.Controls.Add(this.AppFileSize);
+            this.Controls.Add(this.BootFileSize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bin文件合并";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox BootFileSize;
+        private System.Windows.Forms.TextBox AppFileSize;
+        private System.Windows.Forms.TextBox CRCSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBBootFolder;
+        private System.Windows.Forms.Button btBoot;
+        private System.Windows.Forms.Button btApp;
+        private System.Windows.Forms.TextBox tBAppFolder;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btCombineFiles;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton CRC200;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton CRC512;
+        private System.Windows.Forms.Label labelhide;
     }
 }
 
--- CombineBinFilesTool/CombineBinFilesTool/Form1.cs
+++ CombineBinFilesTool/CombineBinFilesTool/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +12,378 @@ namespace CombineBinFilesTool
 {
     public partial class Form1 : Form
     {
+
+        //TextBox的默认值
+        private const String Boot_DEFAULT_TEXT = "128";
+        private const String App_DEFAULT_TEXT = "512";
+        private const String CRC_DEFAULT_TEXT = "40";
+        private  string CrcFileFold = "";
+        private string BootFileTemp = "";
+        private string AppFileTemp = "";
         public Form1()
         {
             InitializeComponent();
+            CommonUsages.Init();
+
+            SetDefaultText(BootFileSize, Boot_DEFAULT_TEXT);
+            SetDefaultText(AppFileSize, App_DEFAULT_TEXT);
+            SetDefaultText(CRCSize, CRC_DEFAULT_TEXT);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btBoot_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "bin文件|*.bin";
+            ofd.Title = "请选择bin文件";
+
+            ofd.InitialDirectory = CommonUsages.DefaultFolder;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tBBootFolder.Text = ofd.FileName;
+                CommonUsages.DefaultFolder = Path.GetFullPath(ofd.FileName);
+            }
+            //if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    tBBootFolder.Text = folderBrowserDialog1.SelectedPath;
+            //}
+        }
+
+        private void btApp_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "bin文件|*.bin";
+            ofd.Title = "请选择bin文件";
+            ofd.InitialDirectory = CommonUsages.DefaultFolder;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tBAppFolder.Text = ofd.FileName;
+                CommonUsages.DefaultFolder = Path.GetFullPath(ofd.FileName);
+            }
+            //if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    tBAppFolder.Text = folderBrowserDialog1.SelectedPath;
+            //}
+        }
+
+        private void btCombineFiles_Click(object sender, EventArgs e)
+        {
+            //CRC校验大小，我看STM32和RTMIM使用的校验值不同。一个是200K，一个是512K
+            int Length = 0;
+            if (CRC200.Checked)
+            {
+                Length = 200 * 1024;
+            }
+            else if (CRC512.Checked)
+            {
+                Length = 512 * 1024;
+            }
+            //string sdg3256 = @"C:\Users\user\Desktop\BIN文件合并\最终的文件\bin_更新\bin\lcm.bin";
+            //if (CommonUsages.CheckCrc16(sdg3256, Length))
+            //{
+            //    string sdg = CommonUsages.CrcString;
+            //}
+
+            //文件校验
+            if (CheckFile())
+            {
+                //填充bin文件到指定大小
+                if (!FillFile(tBBootFolder.Text, long.Parse(BootFileSize.Text.Trim()) * 1024))
+                {
+                    CommonUsages.MyMsgBox(" boot文件夹下bin文件拼接失败", CommonUsages.MsgBoxTypeEnum.Error);
+                    return;
+                }
+                if (!FillFile(tBAppFolder.Text, long.Parse(AppFileSize.Text.Trim()) * 1024))
+                {
+                    CommonUsages.MyMsgBox(" app文件夹下bin文件拼接失败", CommonUsages.MsgBoxTypeEnum.Error);
+                    return;
+                }
+
+                //CRC校验,只校验app地下的，还是扩展拼接后的
+                string CrcFileTemp = Path.GetDirectoryName(tBAppFolder.Text) + @"\" + Path.GetFileNameWithoutExtension(tBAppFolder.Text) + "_temp.bin";
+                if (!CommonUsages.CheckCrc16(CrcFileTemp, Length))
+                {
+                    CommonUsages.MyMsgBox(" app文件夹下bin文件crc校验失败", CommonUsages.MsgBoxTypeEnum.Error);
+                    return;
+                }
+                //string sdg3256 = @"C:\Users\user\Desktop\BIN文件合并\最终的文件\bin_更新\bin\lcm.bin";
+                //if (CommonUsages.CheckCrc16(sdg3256, Length))
+                //{
+                //    string sdg = CommonUsages.CrcString;
+                //}
+
+
+                //crc文件需要新建
+                if (!CreatCrcFile(Path.GetDirectoryName(tBAppFolder.Text)))
+                {
+                    CommonUsages.MyMsgBox(" crc文件创建失败", CommonUsages.MsgBoxTypeEnum.Error);
+                    return;
+
+                }
+
+
+                //合并文件
+                if (!CombineFiles())
+                {
+                    CommonUsages.MyMsgBox(" 合并文件失败", CommonUsages.MsgBoxTypeEnum.Error);
+                    return;
+                }
+                else
+                {
+                    CommonUsages.MyMsgBox(" 合并文件成功", CommonUsages.MsgBoxTypeEnum.Info);
+                    return;
+                }
+            }
+
+        }
+        private bool CheckFile()
+        {
+            //校验文件大小
+            if (File.Exists(tBBootFolder.Text))
+            {
+                if (GetFileSize(tBBootFolder.Text) > double.Parse(BootFileSize.Text))
+                {
+                    CommonUsages.MyMsgBox(" boot loader文件夹下bin文件超过指定大小", CommonUsages.MsgBoxTypeEnum.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                CommonUsages.MyMsgBox("boot loader文件夹下bin文件文件不存在", CommonUsages.MsgBoxTypeEnum.Error);
+                return false;
+            }
+
+            if (File.Exists(tBAppFolder.Text))
+            {
+                if (GetFileSize(tBAppFolder.Text) > double.Parse(AppFileSize.Text))
+                {
+                    CommonUsages.MyMsgBox(" app文件夹下bin文件超过指定大小", CommonUsages.MsgBoxTypeEnum.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                CommonUsages.MyMsgBox("app文件夹下bin文件文件不存在", CommonUsages.MsgBoxTypeEnum.Error);
+                return false;
+            }
+            //CRC校验,只校验app地下的，还是扩展拼接后的
+            //CommonUsages.CheckCrc16(tBBootFolder.Text);
+            return true;
+        }
+        private double GetFileSize(string file)
+        {
+            if (File.Exists(file))
+            {
+                return new FileInfo(file).Length / 1024.00;
+            }
+            else
+                return 0.00;
+
+        }
+
+        private bool FillFile(string fileName, long length)
+        {
+            
+            string FileTemp = Path.GetDirectoryName(fileName) + @"\" + Path.GetFileNameWithoutExtension(fileName) + "_temp.bin";
+            
+            if (!File.Exists(FileTemp))
+            {
+                //File.Create(FileTemp);
+                File.Copy(fileName, FileTemp);
+            }
+            else
+            {
+                File.Delete(FileTemp);
+                File.Copy(fileName, FileTemp);
+            }
+            //string saveFileName = fileName;
+            using (FileStream fs = new FileStream(FileTemp, FileMode.Append, FileAccess.Write))
+            {
+                //方法1
+                //byte[] byte124 = System.Text.Encoding.ASCII.GetBytes(0xff);
+
+
+                //byte[] byte1 = System.Text.Encoding.ASCII.GetBytes("12345678");
+                //fs.Position = 0;
+                //fs.Write(byte1, 0, byte1.Length);
+
+                //byte[] byte2 = System.Text.Encoding.ASCII.GetBytes("abcdef");
+                //fs.Position = byte1.Length;
+                //fs.Write(byte2, 0, byte2.Length);
+
+                //byte[] byte3 = System.Text.Encoding.ASCII.GetBytes("ABCDEF");
+                //fs.Position = byte1.Length + byte2.Length;
+                //fs.Write(byte3, 0, byte3.Length);
+
+                //方法2
+                long dataLength = length - fs.Length;
+                BinaryWriter bw = new BinaryWriter(fs);
+                // bw.BaseStream.Length
+                while (fs.Length < length)
+                {
+                    bw.Write(0xFFFFFFFF);
+
+
+                }
+
+            }//end using
+            return true;
+        }
+
+        private void SetDefaultText(TextBox textBox, string defaultaValue)
+        {
+            textBox.Text = defaultaValue;
+            textBox.ForeColor = Color.Gray;
+        }
+
+        private bool CreatCrcFile(string Directory)
+        {
+            try
+            {
+                string FileTemp = Directory + @"\" + "crc.bin";
+
+                using (FileStream fs = new FileStream(FileTemp, FileMode.Create, FileAccess.Write))
+                {
+                    BinaryWriter bw = new BinaryWriter(fs);
+                    bw.Write((ushort)0xffff);
+                }
+                //string saveFileName = fileName;
+                using (FileStream fs = new FileStream(FileTemp, FileMode.Append, FileAccess.Write))
+                {
+                    BinaryWriter bw = new BinaryWriter(fs);
+
+                    //存在文件中的时候低8为在前，高8为在后
+                    //string CrcValue = CommonUsages.CrcString.Split('_')[1] + CommonUsages.CrcString.Split('_')[0];
+                    //uint crc = uint.Parse(CrcValue);
+                    //bw.Write(crc);
+                    bw.Write(CommonUsages.CrcByte);
+
+                    //方法2
+                    long dataLength = int.Parse(CRCSize.Text.Trim()) * 1024 - fs.Length;                  
+                    // bw.BaseStream.Length
+                    while (fs.Length < dataLength)
+                    {
+                        bw.Write(0xFFFFFFFF);
+                    }
+
+                }//end using
+                CrcFileFold = FileTemp;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                CommonUsages.MyMsgBox(ex.Message, CommonUsages.MsgBoxTypeEnum.Error);
+                return false;
+            }
+            
+        }
+
+        private bool CombineFiles()
+        {
+            try
+            {
+                
+                string fileBootFolder = Path.GetDirectoryName(tBBootFolder.Text) + @"\" + Path.GetFileNameWithoutExtension(tBBootFolder.Text) + "_temp.bin"; 
+                string fileApp = Path.GetDirectoryName(tBAppFolder.Text) + @"\" + Path.GetFileNameWithoutExtension(tBAppFolder.Text) + "_temp.bin"; 
+                string fileCrc = CrcFileFold;
+                string targetpath = Path.GetDirectoryName(tBAppFolder.Text) + @"\" + "boot_app_crc.bin";
+                
+                FileStream fsr1 = new FileStream(fileBootFolder, FileMode.Open, FileAccess.Read);
+                FileStream fsr2 = new FileStream(fileApp, FileMode.Open, FileAccess.Read);
+                FileStream fsr3 = new FileStream(fileCrc, FileMode.Open, FileAccess.Read);
+                byte[] btArr = new byte[fsr1.Length + fsr2.Length + fsr3.Length];
+                fsr1.Read(btArr, 0, Convert.ToInt32(fsr1.Length));
+                fsr2.Read(btArr, Convert.ToInt32(fsr1.Length), Convert.ToInt32(fsr2.Length));
+                fsr3.Read(btArr, Convert.ToInt32(fsr1.Length)+ Convert.ToInt32(fsr2.Length), Convert.ToInt32(fsr3.Length));
+                fsr1.Close();
+                fsr2.Close();
+                fsr3.Close();
+                FileStream fsw = new FileStream(targetpath, FileMode.Create, FileAccess.Write);
+                fsw.Write(btArr, 0, btArr.Length);
+                fsw.Flush();
+                fsw.Close();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static byte HexStringToByte(string str)
+        {
+            byte bt;
+            if (str != "")
+            {
+                bt = Convert.ToByte(str, 16);
+                return bt;
+            }
+            return 0;
+        }
+
+        #region 事件触发
+        private void BootFileSize_Enter(object sender, EventArgs e)
+        {
+            if (BootFileSize.Text == Boot_DEFAULT_TEXT)
+            {
+                BootFileSize.Text = "";
+                BootFileSize.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void BootFileSize_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(BootFileSize.Text))
+                SetDefaultText(BootFileSize, Boot_DEFAULT_TEXT);
+        }
+
+        private void AppFileSize_Enter(object sender, EventArgs e)
+        {
+            if (AppFileSize.Text == App_DEFAULT_TEXT)
+            {
+                AppFileSize.Text = "";
+                AppFileSize.ForeColor = Color.Black;
+            }
+        }
+
+        private void AppFileSize_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(AppFileSize.Text))
+                SetDefaultText(AppFileSize, App_DEFAULT_TEXT);
+        }
+
+        private void CRCSize_Enter(object sender, EventArgs e)
+        {
+            if (CRCSize.Text == CRC_DEFAULT_TEXT)
+            {
+                CRCSize.Text = "";
+                CRCSize.ForeColor = Color.Black;
+            }
+        }
+
+        private void CRCSize_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(CRCSize.Text))
+                SetDefaultText(CRCSize, CRC_DEFAULT_TEXT);
+        }
+        #endregion
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //为了防止焦点选中文本框，虽然找个控件设置默认焦点
+            label1.Focus();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            labelhide.Select();  //label1的visible属性设为false
+
         }
     }
 }
